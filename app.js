604474928127a5382693d06db3425c35ef1b0e4b
Matheus <bernardes.matheus@outlook.com>
2019-02-18 03:50:23 -0200

    Many updates on ranking app

--- app.js
+++ app.js
@@ -40,11 +40,35 @@ app.get('/api/pts',(req,res)=>{
 })
 
 app.get('/api/projs',(req,res)=>{
-    getPtsProjs(connection,req,res);
+    getQntProjs(connection,req,res);
 })
 
+app.get('/api/weeks',(req,res)=>{
+    getQntWeeks(connection,req,res);
+})
+
+app.get('/api/medals',(req,res)=>{
+    getMedals(connection,req,res);
+})
+
+app.post('/api/projeto',(req,res)=>{
+    inserirProjeto(connection,req,res);
+});
+
+app.post('/api/weekly',(req,res)=>{
+    inserirWeeklyChallenge(connection,req,res);
+});
+
+app.get('/api/fotos',(req,res)=>{
+    getFotos(connection,req,res);
+})
+
+
+
+
+
 function getNomeAlunos(conn,req,res){
-    conn.query('SELECT nome FROM Aluno WHERE Nome IS NOT NULL;',(error,results,fields)=>{
+    conn.query('SELECT nome FROM Aluno;',(error,results,fields)=>{
         if(error){
             return res.json(error);
         } else {
@@ -54,7 +78,7 @@ function getNomeAlunos(conn,req,res){
 };
 
 function getPtsAlunos(conn,req,res){
-    conn.query('SELECT Aluno.pts + Medalha.p2_val + Medalha.p4_val + Medalha.p5_val + Medalha.p7_val + Medalha.p9_val + Medalha.s2_val + Medalha.s4_val + Medalha.s6_val + Medalha.s8_val + Medalha.s10_val + Medalha.c1_val + Medalha.c2_val + Medalha.c3_val + Medalha.c4_val + Medalha.c5_val AS ptsTotais FROM Aluno INNER JOIN Medalha ON Aluno.id_medalha = Medalha.id;',(error,results,fields)=>{
+    conn.query('SELECT proj1 + proj2 + proj3 + proj4 + proj5 + proj6 + proj7 + proj8 + proj9 + proj10 + proj11 + proj12 + proj13 + proj14 + proj15 + proj16 + Weekly.week1 + Weekly.week2 + Weekly.week3 + Weekly.week4 + Weekly.week5 + Weekly.week6 + Weekly.week7 + Weekly.week8 + Weekly.week9 + Weekly.week10 + Weekly.week11 + Weekly.week12 + Weekly.week13 + Weekly.week14 + Weekly.week15 + Weekly.week16 + Weekly.week17 + Medalha.p2_val + Medalha.p4_val + Medalha.p5_val + Medalha.p7_val + Medalha.p9_val + Medalha.s2_val + Medalha.s4_val + Medalha.s6_val + Medalha.s8_val + Medalha.s10_val + Medalha.c1_val + Medalha.c2_val + Medalha.c3_val + Medalha.c4_val + Medalha.c5_val AS TOTAL FROM Projeto INNER JOIN Weekly ON Projeto.id = Weekly.id INNER JOIN Medalha ON Weekly.id = Medalha.id ORDER BY TOTAL DESC;',(error,results,fields)=>{
         if(error){
             return res.json(error);
         } else {
@@ -63,8 +87,8 @@ function getPtsAlunos(conn,req,res){
     })
 };
 
-function getPtsProjs(conn,req,res){
-    conn.query('SELECT proj FROM Aluno WHERE proj IS NOT NULL;',(error,results,fields)=>{
+function getQntProjs(conn,req,res){
+    conn.query('SELECT projs FROM Projeto;',(error,results,fields)=>{
         if(error){
             return res.json(error);
         } else {
@@ -72,5 +96,61 @@ function getPtsProjs(conn,req,res){
         }
     })
 };
+
+function getQntWeeks(conn,req,res){
+    conn.query('SELECT weeklys FROM Weekly;',(error,results,fields)=>{
+        if(error){
+            return res.json(error);
+        } else {
+            res.json(results);
+        }
+    })
+};
+
+function getMedals(conn,req,res){
+    conn.query('SELECT p2,p4,p5,p7,p9,s2,s4,s6,s8,s10,c1,c2,c3,c4,c5 FROM Medalha INNER JOIN Aluno ON Medalha.id = Aluno.id_medalha ORDER BY Aluno.pts DESC;',(error,results,fields)=>{
+        if(error){
+            return res.json(error);
+        } else {
+            res.json(results);
+        }
+    })
+};
+
+function inserirProjeto(conn,req,res){
+    let id = req.body.nome;
+    let nProj = req.body.proj;
+    let pts = req.body.pts;
+    conn.query(`UPDATE Projeto SET proj${nProj.toString()} = proj${nProj.toString()} + ${pts} WHERE id = ${id};`,[id,pts],(error,results,fields)=>{
+        if(error){
+            return res.json(error);
+        } else {
+            res.render('index');
+        }
+    })
+};
+
+function inserirWeeklyChallenge(conn,req,res){
+    let id = req.body.nome;
+    let nWeek = req.body.week;
+    let pts = req.body.pts;
+    conn.query(`UPDATE Weekly SET week${nWeek.toString()} = week${nWeek.toString()} + ${pts} WHERE id = ${id};`,[id,pts],(error,results,fields)=>{
+        if(error){
+            return res.json(error);
+        } else {
+            res.render('index');
+        }
+    })
+};
+
+function getFotos(conn,req,res){
+    conn.query('SELECT img_ref FROM Aluno;',(error,results,fields)=>{
+        if(error){
+            return res.json(error);
+        } else {
+            res.json(results);
+        }
+    })
+}
 
 app.listen(PORT,()=>{console.log(`Rodando na porta ${PORT}!`)});
--- public/index.js
+++ public/index.js
@@ -23,6 +23,21 @@ fetch('/api/nomes')
     }
 });
 
+fetch('/api/fotos')
+.then((response) => response.json())
+.then(data => {
+    let fotos = data;
+    for (i=0; i < 3; i++) {
+        let moldura = document.getElementById(`${i.toString()}`+'-img');
+        let foto = createNode('img');
+        foto.className="img";
+        foto.src = `${fotos[i].img_ref}`;
+        console.log(fotos);
+        console.log(foto);
+        append(moldura,foto);
+    }
+});
+
 fetch('/api/pts')
 .then((response) => response.json())
 .then(data => {
@@ -32,7 +47,7 @@ fetch('/api/pts')
         console.log(pts);
         let num = createNode('p');
         num.className="numb";
-        num.innerHTML = `${numeros[i].ptsTotais}`;
+        num.innerHTML = `${numeros[i].TOTAL}`;
         append(pts,num);
     }
 });
@@ -42,25 +57,126 @@ fetch('/api/projs')
 .then(data => {
     let projs = data;
     for (i=0; i < 3; i++) {
-        let pts = document.getElementById(`${i.toString()}`+'-proj');
-        console.log(pts);
+        let qnt = document.getElementById(`${i.toString()}`+'-proj');
+        console.log(qnt);
         let proj = createNode('p');
         proj.className="projs";
-        proj.innerHTML = 'Projetos entregues:'+`${projs[i].proj}`;
-        append(pts,proj);
+        proj.innerHTML = 'Projetos entregues: '+`${projs[i].projs}`;
+        append(qnt,proj);
     }
 });
 
 fetch('/api/weeks')
 .then((response) => response.json())
 .then(data => {
-    let numeros = data;
+    let weeklys = data;
     for (i=0; i < 3; i++) {
-        let pts = document.getElementById(`${i.toString()}`+'-num');
-        console.log(pts);
-        let num = createNode('p');
-        num.className="numb";
-        num.innerHTML = `${numeros[i].ptsTotais}`;
-        append(pts,num);
+        let qnt = document.getElementById(`${i.toString()}`+'-week');
+        console.log(qnt);
+        let week = createNode('p');
+        week.className="weeks";
+        week.innerHTML = 'Weekly Challenges entregues: '+`${weeklys[i].weeklys}`;
+        append(qnt,week);
+    }
+});
+
+
+fetch('/api/medals')
+.then((response) => response.json())
+.then(data => {
+    let medals = data;
+    for (i=0; i < 3; i++) {
+        let mRow = document.getElementById(`${i.toString()}`+'-mrow');
+        
+            if(medals[i].p2===1){
+                let imgP2 = createNode('img');
+                imgP2.className="medal";
+                imgP2.src='/p2.png';
+                append(mRow,imgP2);
+            }
+            if(medals[i].p4===1){
+                let imgP4 = createNode('img');
+                imgP4.className="medal";
+                imgP4.src='/p4.png';
+                append(mRow,imgP4);
+            }
+            if(medals[i].p5===1){
+                let imgP5 = createNode('img');
+                imgP5.className="medal";
+                imgP5.src='/p5.png';
+                append(mRow,imgP5);
+            }
+            if(medals[i].p7===1){
+                let imgP7 = createNode('img');
+                imgP7.className="medal";
+                imgP7.src='/p7.png';
+                append(mRow,imgP7);
+            }
+            if(medals[i].p9===1){
+                let imgP9 = createNode('img');
+                imgP9.className="medal";
+                imgP9.src='/p9.png';
+                append(mRow,imgP9);
+            }
+            if(medals[i].s2===1){
+                let imgS2 = createNode('img');
+                imgS2.className="medal";
+                imgS2.src='/s2.png';
+                append(mRow,imgS2);
+            }
+            if(medals[i].s4===1){
+                let imgS4 = createNode('img');
+                imgS4.className="medal";
+                imgS4.src='/s4.png';
+                append(mRow,imgS4);
+            }
+            if(medals[i].s6===1){
+                let imgS6 = createNode('img');
+                imgS6.className="medal";
+                imgS6.src='/s6.png';
+                append(mRow,imgS6);
+            }
+            if(medals[i].s8===1){
+                let imgS8 = createNode('img');
+                imgS8.className="medal";
+                imgS8.src='/s8.png';
+                append(mRow,imgS8);
+            }
+            if(medals[i].s10===1){
+                let imgS10 = createNode('img');
+                imgS10.className="medal";
+                imgS10.src='/s10.png';
+                append(mRow,imgS10);
+            }
+            if(medals[i].c1===1){
+                let imgC1 = createNode('img');
+                imgC1.className="medal";
+                imgC1.src='/c1.png';
+                append(mRow,imgC1);
+            }
+            if(medals[i].c2===1){
+                let imgC2 = createNode('img');
+                imgC2.className="medal";
+                imgC2.src='/c2.png';
+                append(mRow,imgC2);
+            }
+            if(medals[i].c3===1){
+                let imgC3 = createNode('img');
+                imgC3.className="medal";
+                imgC3.src='/c3.png';
+                append(mRow,imgC3);
+            }
+            if(medals[i].c4===1){
+                let imgC4 = createNode('img');
+                imgC4.className="medal";
+                imgC4.src='/c4.png';
+                append(mRow,imgC4);
+            }
+            if(medals[i].c5===1){
+                let imgC5 = createNode('img');
+                imgC5.className="medal";
+                imgC5.src='/c5.png';
+                append(mRow,imgC5);
+            }
     }
 });
